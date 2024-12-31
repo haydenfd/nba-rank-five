6164b0817376c6831349ee57b6948c6d49d0d69c
Hayden D’Souza <haydenfds@gmail.com>
2024-12-30 20:08:39 -0800

    formatting guide modal

--- src/Components/Modals/GuideModal.tsx
+++ src/Components/Modals/GuideModal.tsx
@@ -20,11 +20,27 @@ export const GuideModal: React.FC<GenericModalsPropsInterface> = ({ isOpen, onOp
           <>
             <ModalHeader className="flex flex-col gap-1 underline italic">How to play</ModalHeader>
             <ModalBody className="text-lg">
-              <span className="block">You have to rank <span className="font-bold">6</span> random NBA players by <span className="font-bold">Points Per Game</span>.</span>
-              <span className="block"> You must rank the players in descending order, from <span className="font-bold">Highest to Lowest</span>.</span>
-              <span className="block">You get <span className="font-bold">3</span> attempts.</span>
+              <ul>
+                <li>
+                <span className="block">You have to rank <span className="font-bold">6</span> random NBA players by <span className="font-bold">Points Per Game</span>.</span>
+                </li>
+                <li>
+                <span className="block"> You must rank the players in descending order, from <span className="font-bold">Highest to Lowest</span>.</span>
+                </li>
+                <li>
+                <span className="block">You get <span className="font-bold">3</span> attempts.</span>
+
+                </li>
+                <li>
               <span className="block">After each attempt, you get feedback on correct and incorrect guesses.</span>
+
+                </li>
+                <li>
               <span className="block">Thanks for playing. Have fun!</span>
+
+                </li>
+              </ul>
+              
             </ModalBody>
           </>
         )}
